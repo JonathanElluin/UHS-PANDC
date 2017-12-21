e2db130e7fd9cd90c0b30f7a14e1565986229c6e
Romain <romain.foray@gmail.com>
2017-12-21 17:09:29 +0100

    commit

--- Point'n'Click/Assets/Scripts/SceneOneManager.cs
+++ Point'n'Click/Assets/Scripts/SceneOneManager.cs
@@ -4,27 +4,13 @@ using UnityEngine;
 
 public class SceneOneManager : MonoBehaviour {
 
-    public GameObject Vildrac;
     private SpriteRenderer VildracSpriteRenderer;
     public GameObject Background;
     private Animator CouloirAnimator;
     private float Alpha;
-
-<<<<<<< HEAD:Point'n'Click/Assets/RoutineScript.cs
-	// Use this for initialization
-	void Start () {
-        VildracSpriteRenderer = Vildrac.GetComponent<SpriteRenderer>();
-        VildracSpriteRenderer.flipX = true;
-        Player_controller.startMoving = true;
-        CouloirAnimator = Background.GetComponent<Animator>();
-	}
-=======
     public GameObject Vildrac;
-
     public GameObject DummyStopPosition;
-
     public GameObject Couloir;
-
     public GameObject Cursor;
     public GameObject VildracAlcolo;
     public GameObject VildracColere;
@@ -37,14 +23,12 @@ public class SceneOneManager : MonoBehaviour {
         CouloirAnimator = Couloir.GetComponent<Animator>();
         CouloirAnimator.SetBool("isOpen", true);
     }
->>>>>>> 966dabaf0299bd93b1f92171b7d4dd03674fe7f5:Point'n'Click/Assets/Scripts/SceneOneManager.cs
 	
 	// Update is called once per frame
 	void Update () {
         
      }
-<<<<<<< HEAD:Point'n'Click/Assets/RoutineScript.cs
-=======
+
 
     private void VildracManager()
     {
@@ -72,5 +56,4 @@ public class SceneOneManager : MonoBehaviour {
         VildracAlcolo.SetActive(true);
         Vildrac.SetActive(false);
     }
->>>>>>> 966dabaf0299bd93b1f92171b7d4dd03674fe7f5:Point'n'Click/Assets/Scripts/SceneOneManager.cs
 }
