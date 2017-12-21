0a3830f9d81c50eeaebe3feec8c4e33a661eb525
Florent <boissy.florent@gmail.com>
2017-12-21 09:55:29 +0100

    création du projet

---
+++ Point'n'Click/Assets/Scripts/SpriteClickScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Assets.Scripts;
+
+public class SpriteClickScript : MonoBehaviour {
+
+    public GameObject sprite;
+
+    public void OnMouseDown()
+    {
+        Debug.Log(gameObject.name);
+        switch (sprite.name)
+        {
+            case "Choix 1":
+                {
+                    SharedObjects.SetInt(gameObject.name, 1);
+                    break;
+                }
+            case "Choix 2":
+                {
+                    SharedObjects.SetInt(gameObject.name, 2);
+                    break;
+                }
+            case "Choix 3":
+                {
+                    SharedObjects.SetInt(gameObject.name, 3);
+                    break;
+                }
+            default:
+                {
+                    SharedObjects.SetInt(gameObject.name, 0);
+                    break;
+                }
+        }
+        Debug.Log(SharedObjects.GetInt(gameObject.name));
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log(gameObject.name);
+    }
+}
