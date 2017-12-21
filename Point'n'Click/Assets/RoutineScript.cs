a01974ed7e8b336290ceaf88cc16bcb827ae1d6f
Amarchy <axel.marchal@hotmail.fr>
2017-12-21 16:38:47 +0100

    Marchy scn 2

--- Point'n'Click/Assets/RoutineScript.cs
+++ Point'n'Click/Assets/RoutineScript.cs
@@ -4,50 +4,22 @@ using UnityEngine;
 
 public class RoutineScript : MonoBehaviour {
 
-
+    public GameObject Vildrac;
     private SpriteRenderer VildracSpriteRenderer;
+    public GameObject Background;
     private Animator CouloirAnimator;
     private float Alpha;
-
-    public GameObject Vildrac;
-
-    public GameObject DummyStopPosition;
-
-    public GameObject Couloir;
-
-    public GameObject Cursor;
-    
 
 	// Use this for initialization
 	void Start () {
         VildracSpriteRenderer = Vildrac.GetComponent<SpriteRenderer>();
-        VildracSpriteRenderer.material.color = new Color(1.0f, 1.0f, 1.0f, Alpha);
         VildracSpriteRenderer.flipX = true;
-        CouloirAnimator = Couloir.GetComponent<Animator>();
-        CouloirAnimator.SetBool("isOpen", true);
-    }
+        Player_controller.startMoving = true;
+        CouloirAnimator = Background.GetComponent<Animator>();
+	}
 	
 	// Update is called once per frame
 	void Update () {
-        VildracManager();
+        
      }
-
-    private void VildracManager()
-    {
-        if (CouloirAnimator.GetCurrentAnimatorStateInfo(0).IsName("PorteOuverte"))
-        {
-            Player_controller.startMoving = true;
-            if (Alpha < 1)
-            {
-                Alpha += 0.05f;
-            }
-            if (Vildrac.transform.position.x <= DummyStopPosition.transform.position.x)
-            {
-                Player_controller.startMoving = false;
-                Player_controller.isIddle = true;
-                Cursor.SetActive(true);
-            }
-            Vildrac.GetComponent<SpriteRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, Alpha);
-        }
-    }
 }
