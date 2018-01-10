595c104af9fac44d80cff6a195c1fb67aa149e28
Amarchy <axel.marchal@hotmail.fr>
2018-01-10 17:12:39 +0100

    Dialog box

--- Point'n'Click/Assets/Scripts/SceneTwo/TextBoxManager.cs
+++ Point'n'Click/Assets/Scripts/SceneTwo/TextBoxManager.cs
@@ -6,7 +6,7 @@ public class TextBoxManager : MonoBehaviour
 {
 
     public GameObject textBox;
-
+    public static TextBoxManager instance;
     public Text theText;
 
     public TextAsset textfile;
@@ -16,6 +16,20 @@ public class TextBoxManager : MonoBehaviour
     public int endAtLine;
 
     public PlayerController player;
+
+    public bool isActive;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Use this for initialization
     void Start()
@@ -37,11 +51,40 @@ public class TextBoxManager : MonoBehaviour
 
     }
 
+    
+
     void Update()
     {
-
         theText.text = textLines[currentLine];
+
+        if (Input.GetKeyDown(KeyCode.Return) && isActive)
+        {
+            currentLine += 1;
+
+        }
+
+        if(currentLine > endAtLine)
+        {
+            DisableTextBox();
+
+        }
+    }
+
+    public void EnableTextBox()
+    {
+        isActive = true;
+        textBox.SetActive(true);
 
     }
 
+    public void DisableTextBox()
+    {
+        isActive = false;
+        textBox.SetActive(false);
+    }
+
+    public static TextBoxManager getInstance()
+    {
+        return instance;
+    }
 }
