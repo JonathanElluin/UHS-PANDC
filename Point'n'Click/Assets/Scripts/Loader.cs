1fb2cd35f10a87f57e3c495f60baabafa543d502
Amarchy <axel.marchal@hotmail.fr>
2018-01-24 13:42:29 +0100

    Scène 2

--- Point'n'Click/Assets/Scripts/Loader.cs
+++ Point'n'Click/Assets/Scripts/Loader.cs
@@ -7,8 +7,9 @@ public class Loader : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+        //PlayerPrefs.DeleteAll();
        int scene = PlayerPrefs.GetInt("SceneToLoad",1);
         SceneManager.LoadScene(scene);
     }
-	
 }
--- Point'n'Click/Assets/Scripts/SceneTwo/RoutineScriptScene2.cs
+++ Point'n'Click/Assets/Scripts/SceneTwo/RoutineScriptScene2.cs
@@ -5,22 +5,81 @@ using UnityEngine;
 public class RoutineScriptScene2 : MonoBehaviour {
 
     public GameObject Vildrac;
+    public Transform[] DummyStopsPosition;
+    private int currentPosition = 0;
+    public TextBoxManager myDialogManager;
+    private Animator VildracAnimator;
     private SpriteRenderer VildracSpriteRenderer;
     public GameObject Background;
     private Animator BureauAnimator;
     private float Alpha;
 
     // Use this for initialization
+
+    private void Awake()
+    {
+        VildracAnimator = Vildrac.GetComponent<Animator>();
+        VildracSpriteRenderer = Vildrac.GetComponent<SpriteRenderer>();
+        VildracSpriteRenderer.material.color = new Color(1.0f, 1.0f, 1.0f, 0);
+        myDialogManager.DisableTextBox();
+        InitSprites();
+    }
+
     void Start()
     {
-        VildracSpriteRenderer = Vildrac.GetComponent<SpriteRenderer>();
-        //VildracSpriteRenderer.flipX = true;
-        BureauAnimator = Background.GetComponent<Animator>();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (VildracAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle_vildrac_alcolo") || VildracAnimator.GetCurrentAnimatorStateInfo(0).IsName("Idle_vildrac_colere") && currentPosition == 0)
+        {
+            Player_controller.startMovingLeft = true;
+           
+        }
+        if (Player_controller.startMovingLeft && currentPosition == 0)
+        {
+            if (Alpha <= 1)
+            {
+                Alpha += 0.02f;
+            }
+            VildracSpriteRenderer.material.color = new Color(1.0f, 1.0f, 1.0f, Alpha);
+            if (Vildrac.transform.position.x <= DummyStopsPosition[currentPosition].position.x)
+            {
+                myDialogManager.EnableTextBox();
+                Player_controller.startMovingLeft = false;
+                Player_controller.isIddle = true;
+                currentPosition++;
+            }
+         
+        }
 
+        if (!myDialogManager.isActive && currentPosition == 1)
+        {
+            Player_controller.isIddle = false;
+            Player_controller.startMovingLeft = true;
+            if (Vildrac.transform.position.x <= DummyStopsPosition[currentPosition].position.x)
+            {
+                myDialogManager.EnableTextBox();
+                Player_controller.startMovingLeft = false;
+                Player_controller.isIddle = true;
+            }
+        }
+    }
+
+    public void InitSprites()
+    {
+        switch(PlayerPrefs.GetInt("ChoiceScene1",0))
+        {
+            case 0:
+                VildracAnimator.SetBool("isAlcolo", true);
+                VildracAnimator.SetBool("isColere", false);
+                break;
+            case 1:
+                VildracAnimator.SetBool("isAlcolo", false);
+                VildracAnimator.SetBool("isColere", true);
+                break;
+        }
     }
 }
