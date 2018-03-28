97fc180e162c8515d972b392aad569415cfed534
Amarchy <axel.marchal@hotmail.fr>
2018-03-28 15:44:33 +0200

    Souris

---
+++ Point'n'Click/Assets/Scripts/ClickSourie.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ClickSourie : MonoBehaviour
+{
+
+    public GameObject Mouse;
+    public Transform Spawn;
+    private bool isInstantiate;
+
+
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D In)
+    {
+
+        if (In.gameObject.tag == "Cursor")
+        {
+            if (!isInstantiate)
+            {
+                Instantiate(Mouse, Spawn.position, Spawn.rotation);
+                isInstantiate = true;
+            }
+
+        }
+
+    }
+
+}
